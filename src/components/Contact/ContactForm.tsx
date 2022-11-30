6fe46df5a3384a3def2539e1cd559a01041711ad
Arturo Leone <arturo.leonesoprasteria.com>
2022-11-30 22:54:18 +0100

    changed back + form labels

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -9,16 +9,13 @@ const ContactForm: FC<Props> = ({ onSubmit }) => {
     <form onSubmit={() => console.log("onSubmit")}>
     <div className="form_column">
       <div className="field">
-        <input type="text" name="name" placeholder=" " required />
-        <label htmlFor="name">First Name</label>
+        <input type="text" name="name" placeholder="First Name" required />
       </div>
       <div className="field">
-        <input type="text" name="lastName" placeholder=" " required />
-        <label htmlFor="name">Last Name</label>
+        <input type="text" name="lastName" placeholder="Last Name" required />
       </div>
       <div className="field">
-        <input type="email" name="mail" placeholder=" " required />
-        <label htmlFor="mail">Email</label>
+        <input type="email" name="mail" placeholder="Email" required />
       </div>
     </div>
     <div className="form_column">
