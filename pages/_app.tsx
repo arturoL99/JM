3f38bcb67f3d46002ad51290606a57d0a86b3d0f
arturoL99 <arturoleone01@gmail.com>
2024-08-20 23:05:49 +0200

    added speedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,7 @@ import "../styles/test.scss";
 import type { AppProps } from "next/app";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -47,6 +48,7 @@ export default function App({ Component, pageProps }: AppProps) {
       >
           <Component {...pageProps} />
       </motion.div>
+      <SpeedInsights />
     </AnimatePresence>
   );
 }
